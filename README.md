d67935202c84edd1ee053700d2ce4e784677bcc7
ahonn <ahonn95@outlook.com>
2016-02-12 20:32:01 +0800

    Update README

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 ## LuooMusic 
 
-落网音乐命令行播放器。基于 python ，需要使用 `BeautifulSoup`, `requests` 模块。Just for fun～
+落网音乐命令行播放器。
+
+基于 python ，需要使用 `BeautifulSoup`, `requests` 模块。
+
+Just for fun～
+
+![LuooMusic 期刊列表](http://7xqvel.com1.z0.glb.clouddn.com/QQ20160212-1%402x.png)
+
+![LuooMusic 播放列表](http://7xqvel.com1.z0.glb.clouddn.com/QQ20160212-0%402x.png)
 
 ##### 安装：
 `$ sudo python setup.py install`
@@ -10,13 +18,14 @@
 
 ##### 基本操作:
 
-键   | 功能
-----:|:---------
-m    | 回到菜单
-l    | 前进
-h    | 后退
-k    | 上移
-j    | 下移
-[    | 上一首
-]    | 下一首
-空格	 | 播放/暂停
+键    | 功能
+:----:|:--------
+m     | 回到菜单
+l     | 前进
+h     | 后退
+k     | 上移
+j     | 下移
+[     | 上一首
+]     | 下一首
+space | 播放/暂停
+
--- src/menu.py
+++ src/menu.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 落网 Menu
-'''
+"""
 
 import curses
 import locale
@@ -20,201 +20,201 @@ from ui import UI
 locale.setlocale(locale.LC_ALL, "")
 code = locale.getpreferredencoding()
 
-carousel = lambda left, right, x: left if (x>right) else (right if x<left else x)
+carousel = lambda left, right, x: left if (x > right) else (right if x < left else x)
+
 
 class Menu:
-	def __init__(self):
-		reload(sys)
-		sys.setdefaultencoding('UTF-8')
-		self.datatype = 'menu'
-		self.title = '落网'
-		self.datalist = ['最新期刊', '分类期刊','搜索期刊', '关于']
-		self.offset = 0
-		self.index = 0
-		self.playing = -1
-		self.number = -1
-		self.presentsong = []
-		self.player = Player()
-		self.ui = UI()
-		self.luoo = Luoo()
-		self.screen = curses.initscr()
-		self.screen.keypad(1)
-		self.step = 10
-		self.stack = []
-		
-	def start(self):
-		self.ui.menu(self.datatype, self.title, self.datalist, self.offset, self.index, self.step, self.number, self.playing)
-		self.stack.append([self.datatype, self.title, self.datalist, self.offset, self.index, self.playing])
+    def __init__(self):
+        reload(sys)
+        sys.setdefaultencoding('UTF-8')
+        self.datatype = 'menu'
+        self.title = '落网'
+        self.datalist = ['最新期刊', '分类期刊', '搜索期刊', '关于']
+        self.offset = 0
+        self.index = 0
+        self.playing = -1
+        self.number = -1
+        self.presentsong = []
+        self.player = Player()
+        self.ui = UI()
+        self.luoo = Luoo()
+        self.screen = curses.initscr()
+        self.screen.keypad(1)
+        self.step = 10
+        self.stack = []
 
-		while True:
-			datatype = self.datatype
-			title = self.title
-			datalist = self.datalist
-			offset = self.offset
-			idx = index = self.index
-			playing = self.playing
-			step = self.step
-			stack = self.stack
-			key = self.screen.getch()
-			self.ui.screen.refresh()
+    def start(self):
+        self.ui.menu(self.datatype, self.title, self.datalist, self.offset, self.index, self.step, self.number,
+                     self.playing)
+        self.stack.append([self.datatype, self.title, self.datalist, self.offset, self.index, self.playing])
 
-			if key == ord('q'):
-				break
-			
-			elif key == ord('k'):
-				if datatype == 'songs':
-					length = len(datalist['song'])
-				else:
-					length = len(datalist)
+        while True:
+            datatype = self.datatype
+            title = self.title
+            datalist = self.datalist
+            offset = self.offset
+            idx = index = self.index
+            playing = self.playing
+            step = self.step
+            stack = self.stack
+            key = self.screen.getch()
+            self.ui.screen.refresh()
 
-				if idx == offset:
-					if offset == 0:
-					    continue
-					self.offset -= step
-					self.index = offset - 1
-				else:
-					self.index = carousel(offset, min(length, offset + step) - 1, idx - 1)
+            if key == ord('q'):
+                break
 
-			elif key == ord('j'):
-				if datatype == 'songs':
-					length = len(datalist['song'])
-				else:
-					length = len(datalist)
+            elif key == ord('k'):
+                if datatype == 'songs':
+                    length = len(datalist['song'])
+                else:
+                    length = len(datalist)
 
-				if idx == min(length, offset + step) - 1:
-					if offset + step >= length:
-						continue
-					self.offset += step
-					self.index = offset + step
-				else:
-					self.index = carousel(offset, min(length, offset + step) - 1, idx + 1)
+                if idx == offset:
+                    if offset == 0:
+                        continue
+                    self.offset -= step
+                    self.index = offset - 1
+                else:
+                    self.index = carousel(offset, min(length, offset + step) - 1, idx - 1)
 
-			elif key == ord('l'):
-				if self.datatype == 'songs' or self.datatype == 'about':
-					continue
-				self.ui.loading()
-				self.dispatch(idx)
-				self.index = 0
-				self.offset = 0
-				self.playing = -1
+            elif key == ord('j'):
+                if datatype == 'songs':
+                    length = len(datalist['song'])
+                else:
+                    length = len(datalist)
 
-			elif key == ord('h'):
-				if len(self.stack) == 1:
-					continue
-				up = stack.pop()
-				self.datatype = up[0]
-				self.title = up[1]
-				self.datalist = up[2]
-				self.offset = up[3]
-				self.index = up[4]
-				self.playing = up[5]
+                if idx == min(length, offset + step) - 1:
+                    if offset + step >= length:
+                        continue
+                    self.offset += step
+                    self.index = offset + step
+                else:
+                    self.index = carousel(offset, min(length, offset + step) - 1, idx + 1)
 
-			elif key == ord('f'):
-				self.search()
+            elif key == ord('l'):
+                if self.datatype == 'songs' or self.datatype == 'about':
+                    continue
+                self.ui.loading()
+                self.dispatch(idx)
+                self.index = 0
+                self.offset = 0
+                self.playing = -1
 
-			elif key == ord(' '):
-				self.presentsongs = ['songs', title, datalist, offset, index, playing]
-				self.player.play(datatype, datalist, idx)
+            elif key == ord('h'):
+                if len(self.stack) == 1:
+                    continue
+                up = stack.pop()
+                self.datatype = up[0]
+                self.title = up[1]
+                self.datalist = up[2]
+                self.offset = up[3]
+                self.index = up[4]
+                self.playing = up[5]
 
-			elif key == ord(']'):
-				self.player.next()
-				if datatype == 'songs':
-					self.index = self.player.idx
-				time.sleep(0.1)
+            elif key == ord('f'):
+                self.search()
 
-			elif key == ord('['):
-				self.player.prev()
-				if datatype == 'songs':
-					self.index = self.player.idx
-				time.sleep(0.1)
+            elif key == ord(' '):
+                self.presentsongs = ['songs', title, datalist, offset, index, playing]
+                self.player.play(datatype, datalist, idx)
 
-			elif key == ord('p'):
-				if len(self.presentsongs) == 0:
-					continue
-				self.stack.append([datatype, title, datalist, offset, index, playing])
-				self.datatype = self.presentsongs[0]
-				self.title = self.presentsongs[1]
-				self.datalist = self.presentsongs[2]
-				self.offset = self.presentsongs[3]
-				self.index = self.presentsongs[4]
-				self.playing = self.presentsongs[5]
+            elif key == ord(']'):
+                self.player.next()
+                if datatype == 'songs':
+                    self.index = self.player.idx
+                time.sleep(0.1)
 
-			elif key == ord('m'):
-				if datatype != 'menu':
-					self.stack.append([datatype, title, datalist, offset, index,  playing])
-					self.datatype = self.stack[0][0]
-					self.title = self.stack[0][1]
-					self.datalist = self.stack[0][2]
-					self.offset = 0
-					self.index = 0
+            elif key == ord('['):
+                self.player.prev()
+                if datatype == 'songs':
+                    self.index = self.player.idx
+                time.sleep(0.1)
 
-			self.playing = self.player.idx
-			self.number = self.player.number
-			self.ui.menu(self.datatype, self.title, self.datalist, self.offset, self.index, self.step, self.number, self.playing)
+            elif key == ord('p'):
+                if len(self.presentsongs) == 0:
+                    continue
+                self.stack.append([datatype, title, datalist, offset, index, playing])
+                self.datatype = self.presentsongs[0]
+                self.title = self.presentsongs[1]
+                self.datalist = self.presentsongs[2]
+                self.offset = self.presentsongs[3]
+                self.index = self.presentsongs[4]
+                self.playing = self.presentsongs[5]
 
-		self.player.stop()
-		curses.endwin()
+            elif key == ord('m'):
+                if datatype != 'menu':
+                    self.stack.append([datatype, title, datalist, offset, index, playing])
+                    self.datatype = self.stack[0][0]
+                    self.title = self.stack[0][1]
+                    self.datalist = self.stack[0][2]
+                    self.offset = 0
+                    self.index = 0
 
-	def dispatch(self, idx):
-		luoo = self.luoo
-		datatype = self.datatype
-		title = self.title
-		datalist = self.datalist
-		offset = self.offset
-		index = self.index
-		playing = self.playing
-		self.stack.append([datatype, title, datalist, offset, index, playing])
+            self.playing = self.player.idx
+            self.number = self.player.number
+            self.ui.menu(self.datatype, self.title, self.datalist, self.offset, self.index, self.step, self.number,
+                         self.playing)
 
-		if datatype == 'menu':
-			self.choice(idx)
-		
-		elif datatype == 'vtype':
-			type_id = datalist[idx]["id"]
-			type_name = datalist[idx]["name"]
-			self.datatype = 'vols'
-			self.datalist = luoo.music(type_id)
-			self.title += ' > ' + type_name
+        self.player.stop()
+        curses.endwin()
 
-		elif datatype == 'vols':
-			vol_number = datalist[idx]['number']
-			self.datatype = 'songs'
-			vol = luoo.vol(vol_number)
-			self.datalist = vol
-			self.title += ' > ' + datalist[idx]['name']
+    def dispatch(self, idx):
+        luoo = self.luoo
+        datatype = self.datatype
+        title = self.title
+        datalist = self.datalist
+        offset = self.offset
+        index = self.index
+        playing = self.playing
+        self.stack.append([datatype, title, datalist, offset, index, playing])
 
-	def choice(self, idx):
-		luoo = self.luoo
-		if idx == 0:
-			self.datalist = luoo.music()
-			self.datatype = 'vols'
-			self.title += ' > 最新期刊'
+        if datatype == 'menu':
+            self.choice(idx)
 
-		elif idx == 1:
-			self.datalist = luoo.typelist()
-			self.datatype = 'vtype'
-			self.title += ' > 分类期刊'
+        elif datatype == 'vtype':
+            type_id = datalist[idx]["id"]
+            type_name = datalist[idx]["name"]
+            self.datatype = 'vols'
+            self.datalist = luoo.music(type_id)
+            self.title += ' > ' + type_name
 
-		elif idx == 2:
-			self.search()
+        elif datatype == 'vols':
+            vol_number = datalist[idx]['number']
+            self.datatype = 'songs'
+            vol = luoo.vol(vol_number)
+            self.datalist = vol
+            self.title += ' > ' + datalist[idx]['name']
 
-		elif idx == 3:
-			self.datatype = 'about'
-			self.title += ' > 关于'
+    def choice(self, idx):
+        luoo = self.luoo
+        if idx == 0:
+            self.datalist = luoo.music()
+            self.datatype = 'vols'
+            self.title += ' > 最新期刊'
 
-		self.offset = 0
-		self.index = 0 
+        elif idx == 1:
+            self.datalist = luoo.typelist()
+            self.datatype = 'vtype'
+            self.title += ' > 分类期刊'
 
-	def search(self):
-		luoo = self.luoo
-		ui = self.ui
-		self.stack.append([self.datatype, self.title, self.datalist, self.offset, self.index, self.playing])
-		self.index = 0
-		self.offset = 0
+        elif idx == 2:
+            self.search()
 
-		self.datatype = 'songs'
-		vol = ui.search()
-		self.datalist = vol
-		self.title = 'vol. ' + vol['number'] + ' ' + vol['title']
+        elif idx == 3:
+            self.datatype = 'about'
+            self.title += ' > 关于'
 
+        self.offset = 0
+        self.index = 0
 
+    def search(self):
+        luoo = self.luoo
+        ui = self.ui
+        self.stack.append([self.datatype, self.title, self.datalist, self.offset, self.index, self.playing])
+        self.index = 0
+        self.offset = 0
 
+        self.datatype = 'songs'
+        vol = ui.search()
+        self.datalist = vol
+        self.title = 'vol. ' + vol['number'] + ' ' + vol['title']
