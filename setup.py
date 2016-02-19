ba97cb972759ad5d94b0f7ad5c8c2b7b6609dc2d
ahonn <ahonn95@outlook.com>
2016-02-19 17:46:47 +0800

    update version & README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'nada',
-    version = '0.1.2',
+    version = '0.1.3',
     keywords=['music', 'luoo', 'cli', 'player'],
     description = "Multi music website command line player",
     license = 'MIT License',
