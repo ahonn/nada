5f98060fe0d4f1ef024c540a6dcebcd71175629b
ahonn <ahonn95@outlook.com>
2016-03-18 18:11:37 +0800

    change log

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nada',
-    version='0.1.5',
+    version='0.1.5.1',
     keywords=['music', 'luoo', 'cli', 'player'],
     description="Multi music website command line player",
     license='MIT License',
